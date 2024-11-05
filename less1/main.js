bc5707842227b16f170836af79d03024e131a2f0
vladkolenchuk <kolenchuk09@gmail.com>
2024-11-05 15:51:55 +0200

    new less

---
+++ less1/main.js
@@ -0,0 +1,56 @@
+// #dYQNrBV
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+//
+let hi = 'hello';
+console.log(hi)
+let owl = 'owu';
+console.log(owl)
+let ua = 'ua';
+console.log(ua)
+let number1 = '1';
+console.log(number1)
+let number10 = '10';
+console.log(number10)
+let number999 = '-999';
+console.log(number999)
+let number123 = '123';
+console.log(number123)
+let number314 = '3.14';
+console.log(number314)
+let number27 = '2.7';
+console.log(number27)
+let number16 = '16';
+console.log(number16)
+let tru = 'true';
+console.log(tru)
+let fal = 'false';
+console.log(fal)
+
+
+// #6Qb97gsv
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+let firstName = 'Vladyslav';
+let middleName = 'Volodumurovich';
+let lastName = 'Kolenchuk';
+let person = firstName + ' ' + middleName+ ' '+ lastName;
+console.log(person)
+
+// #4N0y5tufA
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+let a = 100;
+console.log(typeof a)
+let b = '100';
+console.log(typeof b)
+let c = true;
+console.log(typeof c)
+
+// #ruUtWDUI
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+console.log(prompt("Як вас звуть?"))
+console.log(prompt("Як Вас по-батькові?"))
+console.log(prompt("Скільки Вам років?"))
